d872d98df2c6f88f6477df42d77101a91dc4e6c3
Tri Truong <tmtri@tma.com.vn>
2024-08-07 14:09:56 +0700

    add dropdown components

---
+++ src/components/AppDropdownContent.vue
@@ -0,0 +1,35 @@
+<template>
+<transition name="dropdown-content">
+    <div v-if="active" class="dropdown-content">
+        <slot/>
+    </div>
+</transition>
+</template>
+
+<script>
+export default {
+name: "AppDropdownContent",
+inject: ["sharedState"],
+computed: {
+    active() {
+    return this.sharedState.active;
+    }
+}
+};
+</script>
+
+<style>
+.dropdown-content {
+    position: absolute;
+    background: #FFFFFF
+}
+.dropdown-content-enter-active,
+.dropdown-content-leave-active {
+    transition: all 0.2s;
+}
+.dropdown-content-enter,
+.dropdown-content-leave-to {
+    opacity: 0;
+    transform: translateY(-5px);
+}
+</style>
